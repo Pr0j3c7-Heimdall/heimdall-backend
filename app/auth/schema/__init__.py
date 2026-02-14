8ecaa483b29312cc03fa5d6e6b798534bbd27a45
yeeun0702 <kimyeeun0702@naver.com>
2026-02-14 11:10:42 +0900

    [Refactor] schema에 LogoutRequest export 추가

--- app/auth/schema/__init__.py
+++ app/auth/schema/__init__.py
@@ -1,9 +1,10 @@
-from app.auth.schema.request import LoginRequest, RefreshRequest
+from app.auth.schema.request import LoginRequest, LogoutRequest, RefreshRequest
 from app.auth.schema.response import LoginResponse, RefreshResponse
 
 __all__ = [
     "LoginRequest",
     "LoginResponse",
+    "LogoutRequest",
     "RefreshRequest",
     "RefreshResponse",
 ]
