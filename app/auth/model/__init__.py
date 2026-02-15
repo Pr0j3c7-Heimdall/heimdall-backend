9cebea536fb6ba01c95f95bb70705b21a974a8a0
yeeun0702 <kimyeeun0702@naver.com>
2026-02-15 21:20:06 +0900

    [Refactor] Auth model에서 User 제거
    - auth/model/user.py 삭제
    - RefreshToken만 유지

--- app/auth/model/__init__.py
+++ app/auth/model/__init__.py
@@ -1,4 +1,3 @@
 from app.auth.model.refresh_token import RefreshToken
-from app.auth.model.user import User, UserStatus
 
-__all__ = ["RefreshToken", "User", "UserStatus"]
+__all__ = ["RefreshToken"]
