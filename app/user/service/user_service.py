11f49a0dd858cd6c96ef03da549c854eb58b484e
yeeun0702 <kimyeeun0702@naver.com>
2026-02-15 21:46:40 +0900

    [Refactor] 미사용 UserService.get_me() 제거
    - credentials의 User를 직접 사용하므로 get_me() 불필요
    - User import 제거

--- app/user/service/user_service.py
+++ app/user/service/user_service.py
@@ -1,13 +1,8 @@
 from __future__ import annotations
 
-from app.user.model import User
 from app.user.repository import UserRepository
 
 
 class UserService:
     def __init__(self, user_repository: UserRepository):
         self.user_repository = user_repository
-
-    async def get_me(self, user_id: int) -> User | None:
-        """마이페이지 회원정보 조회"""
-        return await self.user_repository.find_by_id(user_id)
